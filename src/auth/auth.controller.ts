52b50dc8791473467d8e73a6ffb95438802e018c
[georgina] <[lunarecho530@gmail.com]>
2023-02-17 16:34:00 +0800

    dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
-    console.log({
-      dto: dto,
-    });
+  signup(@Body() dto: AuthDto) {
+    console.log(dto);
     return this.authService.signup();
   }
 
